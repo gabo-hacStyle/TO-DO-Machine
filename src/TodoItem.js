9e637dba321dbc21baeba62357ed7f98b648ddfd
Gabriel <gabo2023brazil@gmail.com>
2023-01-12 11:21:20 -0500

    First events added

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,11 +2,27 @@ import React, { useLayoutEffect } from "react";
 import './TodoItem.css';
 
 function TodoItem (props) {
+    const onComplete = () => {
+        alert(`Todo completed: ${props.text}`)
+    }
+    const onDelete = () => {
+        alert(`Todo deleted: ${props.text}`)
+    }
     return(    
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>C</span>
+            <span 
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+            >
+                C
+            </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete">X</span>
+            <span 
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >
+                X
+            </span>
         </li>
     )
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,15 @@ import React from "react";
 import  './TodoSearch.css';
 
 function TodoSearch () {
+    const onSearchValueChange = (pedo) =>{
+        console.log(pedo.target.value)
+    }
     return (
-        <input className="TodoSearch" placeholder="Libros" />
+        <input 
+            className="TodoSearch"
+            placeholder="Libros" 
+            onChange={onSearchValueChange}
+        />
     )
 }
 
