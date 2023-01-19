5092808078fa2ed48859d33d327a4686a5ccfdf1
Gabriel <gabo2023brazil@gmail.com>
2023-01-19 11:48:54 -0500

    Composicion de componeentes

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,77 @@
 import React from "react";
-import { AppUi } from "./AppUI";
-import { TodoProvider } from "../TodoContext";
+import { CreateTodoButton } from "../CreateTodoButton";
+import { TodoItem } from "../TodoItem";
+import { TodoList } from "../TodoList";
+import { Modal } from "../Modal";
+import { TodoForm } from "../TodoForm";
+import { EmptyTodos } from "../EmptyTodos";
+import {TodosError} from "../TodosError/index.js"
+import {TodosLoading} from "../TodosLoading/index.js"
+import { TodoHeader } from "../TodoHeader";
+import { TodoCounter } from "../TodoCounter";
+import { TodoSearch } from "../TodoSearch";
+import { useTodos } from "./useTodos"
 
 
 function App() {
+  const {error, 
+    loading, 
+    searchedTodos, 
+    completeTodo, 
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    totalTodos, 
+    completedTodos,
+    searchValue, 
+    setSearchValue,
+    addTodo,
+  } = useTodos();
   return (
-    <TodoProvider>
-      <AppUi />
-    </TodoProvider>
-  )
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter
+          totalTodos={totalTodos}
+          completedTodos={completedTodos}
+        />
+
+        <TodoSearch 
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </TodoHeader>
+         
+      <TodoList>
+        {error && <TodosError error={error} />}
+        {loading && <TodosLoading />}
+        {(!loading && !searchedTodos.length) && <EmptyTodos />}
+        {searchedTodos.map((todo, index) =>  
+          <TodoItem 
+            key={index} 
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        )} 
+      </TodoList>
+
+      {openModal && (
+        <Modal>
+         <TodoForm 
+         addTodo={addTodo}
+         setOpenModal={setOpenModal}
+         />
+        </Modal>
+      )}
+
+      <CreateTodoButton 
+        setOpenModal={setOpenModal}
+        openModal={openModal}
+      />
+     
+    </React.Fragment>
+  );
 }
 
 export default App;
